6445550fc49d505b222a9ab53c226d42bf80af96
landyking <landykingdom@gmail.com>
2023-03-28 16:19:58 +0000

    redesign

---
+++ pages/Management/jsobjects/globalSettings/globalSettings.js
@@ -0,0 +1,46 @@
+export default {
+	myVar2: {},
+	checkBaseURL:async ()=>{
+		if(!appsmith.store.baseURL){
+			await navigateTo("Setup")
+		}
+	},
+	reloadDataAndResetTable:async ()=>{
+		get_single_object.clear()
+		const arr=[];
+		arr.push(all_object_kinds.run())
+		arr.push(list_objects.run())
+		await Promise.all(arr)
+		resetWidget("kinds_table",true)
+		resetWidget("Select1",true)
+	},
+	loadKindAndCount:()=>{
+		const kindAndCount=all_object_kinds.data.map(kind=>{
+			const count=list_objects.data.filter(o=>o.kind==kind).length;
+			return {kind,count}
+		});
+		return kindAndCount;
+	},
+	getKindObjectsOptions:(kind)=>{
+		return list_objects.data
+		.filter(it=>it.kind==kind)
+		.map(it=>({label:it.name,value:it.name}))
+	},
+	apiBaseURL: () => {
+		return appsmith.store.baseURL;
+	},
+	closeModalAndReloadTable:async () => {
+		closeModal("create_modal");
+		await this.reloadDataAndResetTable()
+	},
+	handleObjectChange:async ()=>{
+		get_single_object.clear();
+		const selected=Select1.selectedOptionValue||""
+		if(!_.isEmpty(selected)){
+			const data=await get_single_object.run()
+			return data
+		}else{
+			return ""
+		}
+	}
+}
